4890b338f6056d5fb5f0e67a1bbe751dde2969b5
NTN1789 <natanalmeida040@gmail.com>
2023-06-16 11:59:19 -0300

    first commit

---
+++ src/components/Porta/Portas.tsx
@@ -0,0 +1,12 @@
+import styles from "./Portas.module.scss"
+type PortaProps = {
+
+}
+
+export default function Portas({}: PortaProps ) {
+  return (
+    <div className={styles.porta}>
+     <div className={styles.number}>1</div>
+        </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Portas from "@/components/Porta/Portas";
+
+
+
+
+
+export default function Home() {
+  return (
+    <div >
+        <Portas/>
+    </div>
+  )
+}
